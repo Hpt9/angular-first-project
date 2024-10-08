65b693db962f17c73e00f9e43c7990c9e0dadb12
Farid Sattarov <feridsattarov5@gmail.com>
2024-10-08 06:19:59 +0400

    app-component changed

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component,Input} from '@angular/core';
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required:true}) avatar!:string;
+  @Input({required:true}) name!:string;
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+  onUserClick (){
+    
+  }
+}
